666e3cc6dd80222fda901b261a57dfcd14723dd2
Asad Sayeed <ashasd97@gmail.com>
2023-06-05 21:58:06 +0530

    Daily update

---
+++ ArrayAndString/Remove Duplicates from Sorted Array II.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/?envType=study-plan-v2&envId=top-interview-150
+# Question 4
+# Mon 5 June 2023
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        # Initialize an integer k that updates the kth index of the array...
+        # only when the current element does not match either of the two previous indexes. ...
+        k = 0
+        # Traverse all elements through loop...
+        for i in nums:
+            # If the index does not match elements, count that element and update it...
+            if k < 2 or i != nums[k - 2]:
+                nums[k] = i
+                k += 1
+        return k       # Return k after placing the final result in the first k slots of nums...
